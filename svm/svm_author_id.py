d877ba6a65d86c0d051aec27d903d5722942b396
Frank Sarnelli <FLSarnelli@gmail.com>
2020-05-01 00:30:28 -0500

    SVM Module

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -19,12 +19,25 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
+from sklearn import svm 
 
 
 #########################################################
 ### your code goes here ###
+#print('shinking dataset')
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
+#print('Done!')
 
+print('Starting SVM...')
+clf = svm.SVC(kernel='rbf',C=10000, gamma='auto')
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+print('SVM Done!')
+
+print('10th: ' + str(pred[10]))
+print('26th: ' + str(pred[26]))
+print('50th: ' + str(pred[50]))
+print('Accuracy: ' + str(clf.score(features_test,labels_test)))
+print('Number of Chris Emails: ' + str(list(pred).count(1)))
 #########################################################
-
-
